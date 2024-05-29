0a8e266096a0af9774d629a20532c04db9eac00a
bibhishandhagate <bibhishandhagate@gmail.com>
2024-05-29 10:40:23 +0530

    error file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Bibhishan Dhagate! keep learning");
-		 System.out.println("This is the beauty of the Jenkins Tool");
+		System.out.println("This is the beauty of the Jenkins Tool");
+		System.out.println("This line as error")
     }
 }
